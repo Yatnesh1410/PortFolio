d9e3280422559988ee5f78c715cd5a329668ef30
Yatnesh1410 <yattugyani14@gmail.com>
2022-01-23 13:33:39 +0530

    Build pushed

--- src/components/body/Projects/projects.js
+++ src/components/body/Projects/projects.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import './projects.css';
-import {ProjectData} from './ProjectData/ProjectData.js';
-import ProjectCard from './ProjectCard.js';
-import Button from '@mui/material/Button';
+import React from "react";
+import "./projects.css";
+import { ProjectData } from "./ProjectData/ProjectData.js";
+import ProjectCard from "./ProjectCard.js";
+import Button from "@mui/material/Button";
 function projects() {
-    const data = ProjectData;
+  const data = ProjectData;
 
-    return (
-        <div className = "projects">
-            <label className = "section-title">Projects</label>
+  return (
+    <div className="projects">
+      <label className="section-title">Projects</label>
 
-            {data.map((item) => {
-                return (
-                    <ProjectCard project={item} />
-                );
-            })}
+      {data.map((item) => {
+        return <ProjectCard project={item} />;
+      })}
 
-            <a href = "https://github.com/Yatnesh1410" className = "github-link" target='_blank'><Button variant="contained" className="button">View More</Button></a>
-        </div>
-    )
+      <a
+        href="https://github.com/Yatnesh1410"
+        className="github-link"
+        target="_blank"
+      >
+        <Button variant="contained" className="button">
+          View More
+        </Button>
+      </a>
+    </div>
+  );
 }
 
-export default projects
+export default projects;
